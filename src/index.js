7a4a9550746a1749eada116e018af1bb3c62ba6b
Jan Galinski <jan.galinski@holisticon.de>
2019-05-31 13:14:11 +0200

    extension to tutorial: detect draw

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
+function Square(props) { return (
+    <button className="square" onClick={props.onClick}>
+        {props.value}
+    </button>
+)}
 
   
 class Board extends React.Component {
@@ -49,7 +47,10 @@ class Board extends React.Component {
     render() {
         const winner = calculateWinner(this.state.squares);
         let status;
-        if (winner) {
+        if (winner === 'Draw') {
+            status = winner
+        }
+        else if (winner) {
             status = 'Winner: ' + winner;
         } else {
             status = 'Next player: ' + this.nextPlayer();
@@ -113,11 +114,19 @@ class Board extends React.Component {
       [0, 4, 8],
       [2, 4, 6],
     ];
+    // if a player owns row, column or diagonal, he has won
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
     }
-    return null;
+    // if no one won and at least one square is empty: no winner
+    for (let i=0; i<9; i++) {
+        if (!squares[i]) {
+            return null;
+        }
+    }
+    // on one won and board full: draw
+    return 'Draw';
   }
