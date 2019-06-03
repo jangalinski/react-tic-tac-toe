68c4bb8b6156d1686b47e3edb80cd4aa69699dc4
Jan Galinski <jan.galinski@holisticon.de>
2019-06-04 00:50:44 +0200

    publich from docs/, fixes #7

---
+++ docs/precache-manifest.47a3b01ce5f16f900a335a695d1ae95c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388c29bf42a2bae9cdaf6ef1eb2264d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfbe3f35b449a45554d0",
+    "url": "./static/css/main.06504f8f.chunk.css"
+  },
+  {
+    "revision": "2e6a57bdd34fe92741f3",
+    "url": "./static/js/2.755cb22a.chunk.js"
+  },
+  {
+    "revision": "bfbe3f35b449a45554d0",
+    "url": "./static/js/main.f9115a74.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
